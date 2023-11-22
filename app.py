9beb9556f3f25cec28bf08f6e50625c80fac179d
dwiilhamr7 <129711957+dwiilhamr7@users.noreply.github.com>
2023-11-22 08:58:33 +0000

    update app

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ with conn.session as session:
                                                        symptom text, handphone varchar, address text, tanggal date);')
     session.execute(query)
 
-st.header('SIMPLE HOSPITAL DATA MANAGEMENT October')
+st.header('SIMPLE HOSPITAL DATA MANAGEMENT October-November')
 page = st.sidebar.selectbox("Pilih Menu", ["View Data","Edit Data"])
 
 if page == "View Data":
