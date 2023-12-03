2caf870f0a5ef8080adf4f599d0ade758897a35e
dwiilhamr7 <129711957+dwiilhamr7@users.noreply.github.com>
2023-12-03 13:50:37 +0000

    update app

--- app.sql
+++ app.sql
@@ -18,4 +18,4 @@ CREATE TABLE pelanggan (
 INSERT INTO pelanggan (nama, gender, contact, series_room, other_needs, check_in, time_ci, check_out, time_co, payment, price) 
 VALUES
     ('dr. Nurita', 'male', '0812121', ARRAY['twin deluxe'], 'water', '2023-10-01', '13:00', '2023-10-02', '08:00', 'Tunai', 280000),
-    ('Aldenia Boo', 'female', '0812122018', ARRAY['double bed'], 'water & sandals', '2023-10-02', '14:00', '2023-10-04', '08:00', 'Transfer', 1050000);
+    ('Aldenia Boo', 'female', '0812122018', ARRAY['twin deluxe'], 'water & sandals', '2023-10-02', '14:00', '2023-10-04', '08:00', 'Transfer', 1050000);
