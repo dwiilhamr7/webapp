aa70fcdb584fa9066e6756e33ea85e7be8afb046
dwiilhamr7 <129711957+dwiilhamr7@users.noreply.github.com>
2023-12-03 16:57:49 +0000

    update app

--- app.py
+++ app.py
@@ -8,65 +8,63 @@ list_payment = ['', 'ATM', 'Transfer', 'Tunai']
 conn = st.connection("postgresql", type="sql", 
                      url="postgresql://dwiilhamr07:QBZxK7A6gYND@ep-hidden-unit-18107709.us-east-2.aws.neon.tech/web")
 
-home = st.title('Favorit Hotel in Jawa Timur')
-describe = st.header('Drasri Beautifull Luxury Hotel')
+def main_home():
+    st.title('Favorit Hotel in Jawa Timur')
+    st.header('Drasri Beautiful Luxury Hotel')
+    
+    # Add any content for the main home page
+    # ...
 
-with conn.session as session:
-    query = text('CREATE TABLE IF NOT EXISTS pelanggan (id serial, nama text, gender varchar, contact text, series_room varchar, other_needs text, \
-                                                       check_in date, time_ci time, check_out date, time_co time, payment text, price text);')
-    session.execute(query)
+def sub_home():
+    st.header('Reservation Drasri Beautiful Luxury Hotel')
+    
+    page = st.sidebar.selectbox("Hotel Room", ["View Room","Edit Room"])
 
-st.header('Reservation Drasri Beautifull Luxury Hotel')
+    if page == "View Room":
+        data = conn.query('SELECT * FROM pelanggan ORDER By id;', ttl="0").set_index('id')
+        st.dataframe(data)
 
+    elif page == "Edit Room":
+        if st.button('Tambah Data'):
+            with conn.session as session:
+                query = text('INSERT INTO pelanggan ("nama", "gender", "contact", "series_room", "other_needs", "check_in", "time_ci", "check_out", "time_co", "payment", "price") \
+                            VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11);')
+                session.execute(query, {'1':'', '2':'', '3':'', '4':'', '5':None, '6':None, '7':'', '8':None, '9':'', '10':'', '11':'Rp'})
+                session.commit()
 
-page = st.sidebar.selectbox("Hotel Room", ["View Room","Edit Room"])
+        data = conn.query('SELECT * FROM pelanggan ORDER By id;', ttl="0")
+        for _, result in data.iterrows():
+            st.write(result) 
+            id = result['id']
+            nama_lama = result.loc["nama"]
+            gender_lama = result["gender"]
+            contact_lama = result["contact"]
+            series_room_lama = result["series_room"]
+            other_needs_lama = result["other_needs"]
+            check_in_lama = result["check_in"]
+            time_ci_lama = result["time_ci"]
+            check_out_lama = result["check_out"]
+            time_co_lama = result["time_co"]
+            payment_lama = result["payment"]
+            price_lama = result["price"]
 
-if page == "View Room":
-    data = conn.query('SELECT * FROM pelanggan ORDER By id;', ttl="0").set_index('id')
-    st.dataframe(data)
+        with st.expander(f'a.n. {nama_lama}'):
+            with st.form(f'data-{id}'):
+                nama_baru = st.text_input("nama", nama_lama)
+                gender_baru = st.selectbox("gender", list_gender, list_gender.index(gender_lama))
+                contact_baru = st.text_input("contact", contact_lama)
+                series_room_baru = st.selectbox("series_room", list_room, list_room.index(series_room_lama) if series_room_lama in list_room else 0) 
+                other_needs_baru = st.text_input("other_needs", other_needs_lama)
+                check_in_baru = st.date_input("check_in", check_in_lama)
+                time_ci_baru = st.time_input("time_ci", time_ci_lama)
+                check_out_baru = st.date_input("check_out", check_out_lama)
+                time_co_baru = st.time_input("time_co", time_co_lama)
+                payment_baru = st.selectbox("payment", list_payment, list_payment.index(payment_lama))
+                price_baru = st.text_input("price", price_lama)
 
-if page == "Edit Room":
-    if st.button('Tambah Data'):
-        with conn.session as session:
-            query = text('INSERT INTO pelanggan ("nama", "gender", "contact", "series_room", "other_needs", "check_in", "time_ci", "check_out", "time_co", "payment", "price") \
-                        VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11);')
-            session.execute(query, {'1':'', '2':'', '3':'', '4':'', '5':None, '6':None, '7':'', '8':None, '9':'', '10':'', '11':'Rp'})
-            session.commit()
+                col1, col2 = st.columns([1, 6])
 
-    data = conn.query('SELECT * FROM pelanggan ORDER By id;', ttl="0")
-    for _, result in data.iterrows():
-        st.write(result) 
-        id = result['id']
-        nama_lama = result.loc["nama"]
-        gender_lama = result["gender"]
-        contact_lama = result["contact"]
-        series_room_lama = result["series_room"]
-        other_needs_lama = result["other_needs"]
-        check_in_lama = result["check_in"]
-        time_ci_lama = result["time_ci"]
-        check_out_lama = result["check_out"]
-        time_co_lama = result["time_co"]
-        payment_lama = result["payment"]
-        price_lama = result["price"]
-
-
-    with st.expander(f'a.n. {nama_lama}'):
-        with st.form(f'data-{id}'):
-            nama_baru = st.text_input("nama", nama_lama)
-            gender_baru = st.selectbox("gender", list_gender, list_gender.index(gender_lama))
-            contact_baru = st.text_input("contact", contact_lama)
-            series_room_baru = st.selectbox("series_room", list_room, list_room.index(series_room_lama) if series_room_lama in list_room else 0) 
-            other_needs_baru = st.text_input("other_needs", other_needs_lama)
-            check_in_baru = st.date_input("check_in", check_in_lama)
-            time_ci_baru = st.time_input("time_ci", time_ci_lama)
-            check_out_baru = st.date_input("check_out", check_out_lama)
-            time_co_baru = st.time_input("time_co", time_co_lama)
-            payment_baru = st.selectbox("payment", list_payment, list_payment.index(payment_lama))
-            price_baru = st.text_input("price", price_lama)
-                
-            col1, col2 = st.columns([1, 6])
-
-            with col1:
+                with col1:
                     if st.form_submit_button('UPDATE'):
                         with conn.session as session:
                             query = text('UPDATE pelanggan \
@@ -78,11 +76,16 @@ if page == "Edit Room":
                                                     '12':id})
                             session.commit()
                             st.experimental_rerun()
-                
-            with col2:
+
+                with col2:
                     if st.form_submit_button('DELETE'):
                         query = text(f'DELETE FROM pelanggan WHERE id=:1;')
                         session.execute(query, {'1':id})
                         session.commit()
                         st.experimental_rerun()
 
+# Choose whether to show the main home or sub-home based on user selection
+if st.sidebar.checkbox("Show Sub Home"):
+    sub_home()
+else:
+    main_home()
