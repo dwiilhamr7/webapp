43b48ebec22f4513ae412ed4be12593ab7abe9de
dwiilhamr7 <129711957+dwiilhamr7@users.noreply.github.com>
2023-12-04 13:52:28 +0000

    update app

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ def restaurant_hotel():
     if page == "Additing Data Restaurant":
         if st.button('Tambah Data'):
             with conn.session as session:
-                query = text('INSERT INTO hotel_restaurant ("nama", "makanan", "jumlah_makanan", "minuman", "jumlah_minuman", "metode", "no_tempat", "total_harga", "pembayaran") \
+                query = text('INSERT INTO hotel_restaurant ("pelanggan", "makanan", "jumlah_makanan", "minuman", "jumlah_minuman", "metode", "no_tempat", "total_harga", "pembayaran") \
                         VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9);')
                 session.execute(query, {'1':'', '2':'', '3':None, '4':'', '5':None, '6':'', '7':'', '8':['Rp'], '9':''})
                 session.commit()
@@ -116,7 +116,7 @@ def restaurant_hotel():
     for _, result in data.iterrows():
         st.write(result) 
         id = result['id']
-        nama_lama1 = result.loc["nama"]
+        pelanggan_lama = result.loc["pelanggan"]
         makanan_lama = result["makanan"]
         jumlah_makanan_lama = result["jumlah_makanan"]
         minuman_lama = result["minuman"]
@@ -126,16 +126,16 @@ def restaurant_hotel():
         total_harga_lama = result["total_harga"]
         pembayaran_lama = result["pembayaran"]
 
-    with st.expander(f'a.n. {nama_lama1}'):
+    with st.expander(f'a.n. {pelanggan_lama}'):
         with st.form(f'data-{id}'):
-            nama_baru1 = st.text_input("nama", nama_lama1)
+            pelanggan_baru = st.text_input("pelanggan", pelanggan_lama)
             makanan_baru = st.text_input("makanan", makanan_lama)
             jumlah_makanan_baru = st.text_input("jumlah_makanan", jumlah_makanan_lama)
             minuman_baru = st.text_input("minuman", minuman_lama) 
             jumlah_minuman_baru = st.text_input("jumlah_minuman", jumlah_minuman_lama)
             metode_baru = st.selectbox("metode", list_metode, list_metode.index(metode_lama))
             no_tempat_baru = st.text_input("no_tempat", no_tempat_lama)
-            total_harga_baru = st.date_input("total_harga", total_harga_lama)
+            total_harga_baru = st.text_input("total_harga", total_harga_lama)
             pembayaran_baru = st.selectbox("pembayaran", list_payment, list_payment.index(pembayaran_lama))
                 
             col1, col2 = st.columns([1, 6])
@@ -144,10 +144,10 @@ def restaurant_hotel():
                     if st.form_submit_button('UPDATE'):
                         with conn.session as session:
                             query = text('UPDATE hotel_restaurant \
-                                          SET nama=:1, makanan=:2, jumlah_makanan=:3, minuman=:4, jumlah_minuman=:5 \
+                                          SET pelanggan=:1, makanan=:2, jumlah_makanan=:3, minuman=:4, jumlah_minuman=:5 \
                                           metode=:6, no_tempat=:7, total_harga=:8, pembayaran=:9, \
                                           WHERE id=:10;')
-                            session.execute(query, {'1':nama_baru1, '2':makanan_baru, '3':jumlah_makanan_baru, '4':minuman_baru, '5':jumlah_minuman_baru, 
+                            session.execute(query, {'1':pelanggan_baru, '2':makanan_baru, '3':jumlah_makanan_baru, '4':minuman_baru, '5':jumlah_minuman_baru, 
                                                     '6':metode_baru, '7':no_tempat_baru, '8':total_harga_baru, '9':pembayaran_baru, 
                                                     '10':id})
                             session.commit()
