845fd1946c9cca47d46e05343e0e602b6f29be3b
dwiilhamr7 <129711957+dwiilhamr7@users.noreply.github.com>
2023-12-05 10:56:42 +0000

    update app

--- app.sql
+++ app.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS hotel_room;
+
 CREATE TABLE hotel_room (
     id serial,
     nama text,
@@ -16,26 +17,28 @@ CREATE TABLE hotel_room (
 
 insert into hotel_room  (nama, gender, contact, series_room, other_needs, check_in, time_ci, check_out, time_co, payment, price) 
 values
-	('Arindra Ningtiyas', 'male', '0812121', '["Twin Deluxe"]', 'Water', '2023-10-01', '13:00', '2023-10-02', '08:00', 'Tunai', 280000),
-	('Pahlawan Nur Ihzza', 'male', '0813876', '["Business Premium"]', 'Water dan Sabdals', '2023-10-02', '14:00', '2023-10-04', '09:00', 'Transfer', 850000)
+	('Arindra Ningtiyas', 'male', 0812121, '["Twin Deluxe"]', 'Water', '2023-10-01', '13:00', '2023-10-02', '08:00', 'Tunai', 280000),
+	('Pahlawan Nur Ihzza', 'male', 0813876, '["Business Premium"]', 'Water dan Sabdals', '2023-10-02', '14:00', '2023-10-04', '09:00', 'Transfer', 850000)
 	;
 
-DROP TABLE IF EXISTS restaurant;
+DROP TABLE IF EXISTS hotel_restaurant;
 
-CREATE TABLE restaurant (
+CREATE TABLE hotel_restaurant (
     id serial,
     pelanggan text,
     makanan text,
-    jumlah_makanan integer,
+    jumlah_makanan text,
     minuman text, 
-    jumlah_minuman integer,
+    jumlah_minuman text,
     metode text,
     no_tempat text,
-    total_harga integer,
-    pembayaran text
+    total_harga text,
+    pembayaran text,
+    tanggal date
 );
 
-INSERT INTO restaurant (pelanggan, makanan, jumlah_makanan, minuman, jumlah_minuman, metode, no_tempat, total_harga, pembayaran) 
+INSERT INTO hotel_restaurant (pelanggan, makanan, jumlah_makanan, minuman, jumlah_minuman, metode, no_tempat, total_harga, pembayaran, tanggal) 
 VALUES
-    ('dr. Nurita', 'Mie Ayam Spesial', '2', 'Air Putih', '4', 'Diantar', 'Kamar 203', '60.000', 'Tunai'),
-    ('Aldenia Boo', 'Nasi Goreng Udang, Mie Ayam Bakso', '1, 2', 'Es Jeruk'], '3', 'Ditunggu', 'Meja 8', '84.000', 'ATM');
+    ('dr. Nurita', 'Mie Ayam Spesial', 2, 'Air Putih', 4, 'Diantar', 'Kamar 203', 60000, 'Tunai', '2023-10-02'),
+    ('Aldenia Boo', 'Nasi Goreng Udang, Mie Ayam Bakso', 1, 'Es Jeruk', 3, 'Ditunggu', 'Meja 8', 84000, 'ATM', '2023-10-02')
+   	;
