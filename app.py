fce60ab290ab67ba37c81780fb604f8d83dee786
dwiilhamr7 <129711957+dwiilhamr7@users.noreply.github.com>
2023-12-05 11:51:51 +0000

    update app

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ if page_awal == "Additing Database":
         with conn.session as session:
             query = text('INSERT INTO hotel_room (nama, gender, contact, series_room, other_needs, check_in, time_ci, check_out, time_co, payment, price) \
                             VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11);')
-            session.execute(query, {'1':'', '2':'', '3':'', '4':'[]', '5':None, '6':'', '7':'', '8':'', '9':'', '10':'', '11':''})
+            session.execute(query, {'1':'', '2':'', '3':'', '4':'', '5':None, '6':'', '7':'', '8':'', '9':'', '10':'', '11':''})
             session.commit()
 
     data = conn.query('SELECT * FROM hotel_room ORDER By id;', ttl="0")
@@ -50,7 +50,7 @@ if page_awal == "Additing Database":
                 nama_akhir = st.text_input("nama", nama_awal)
                 gender_akhir = st.selectbox("gender", list_gender, list_gender.index(gender_awal))
                 contact_akhir = st.text_input("contact", contact_awal)
-                room_akhir = st.selectbox("series_room", list_room, list_room.index(room_awal))
+                room_akhir = st.text_input("series_room", room_awal)
                 other_akhir = st.text_input("other_needs", other_awal)
                 checkin_akhir = st.date_input("check_in", checkin_awal)
                 timeci_akhir = st.time_input("time_ci", timeci_awal)
@@ -68,7 +68,7 @@ if page_awal == "Additing Database":
                                           SET nama=:1, gender=:2, contact=:3, series_room=:4, other_needs=:5 \
                                           check_in=:6, time_ci=:7, check_out=:8, time_co=:9, payment=:10, price=:11 \
                                           WHERE id=:12;')
-                            session.execute(query, {'1':nama_akhir, '2':gender_akhir, '3':contact_akhir, '4':str(room_akhir), '5':other_akhir,'6':checkin_akhir, 
+                            session.execute(query, {'1':nama_akhir, '2':gender_akhir, '3':contact_akhir, '4':room_akhir, '5':other_akhir,'6':checkin_akhir, 
                                                     '7':timeci_akhir, '8':checkout_akhir, '9':timeco_akhir, '10':payment_akhir, '11':price_akhir, '12':id})
                             session.commit()
                             st.experimental_rerun()
