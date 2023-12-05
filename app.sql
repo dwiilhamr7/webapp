273aa13568e91a4cdef1a48836a5ffbaea26aa05
dwiilhamr7 <129711957+dwiilhamr7@users.noreply.github.com>
2023-12-05 06:12:35 +0000

    update app

--- app.sql
+++ app.sql
@@ -1,28 +1,28 @@
-DROP TABLE IF EXISTS hotel_room;
+DROP TABLE IF EXISTS room;
 
-CREATE TABLE hotel_room (
+CREATE TABLE room (
     id serial,
     nama text,
     gender text,
     contact text,
-    series_room text[],  -- Menggunakan tipe array untuk series_room
+    series_room text[],
     other_needs text,
     check_in date,
     time_ci time,
     check_out date,
     time_co time,
-    payment text,  -- Menggunakan tipe data text untuk payment
-    price text  -- Menggunakan tipe data numeric untuk price
+    payment text, 
+    price text 
 );
 
-INSERT INTO hotel_room (nama, gender, contact, series_room, other_needs, check_in, time_ci, check_out, time_co, payment, price) 
+INSERT INTO room (nama, gender, contact, series_room, other_needs, check_in, time_ci, check_out, time_co, payment, price) 
 VALUES
     ('dr. Nurita', 'male', '0812121', ARRAY['twin deluxe'], 'water', '2023-10-01', '13:00', '2023-10-02', '08:00', 'Tunai', 280000),
     ('Aldenia Boo', 'female', '0812122018', ARRAY['twin deluxe'], 'water & sandals', '2023-10-02', '14:00', '2023-10-04', '08:00', 'Transfer', 1050000);
 
-DROP TABLE IF EXISTS hotel_restaurant;
+DROP TABLE IF EXISTS restaurant;
 
-CREATE TABLE hotel_restaurant (
+CREATE TABLE restaurant (
     id serial,
     pelanggan text,
     makanan text,
@@ -35,7 +35,7 @@ CREATE TABLE hotel_restaurant (
     pembayaran text
 );
 
-INSERT INTO hotel_restaurant (pelanggan, makanan, jumlah_makanan, minuman, jumlah_minuman, metode, no_tempat, total_harga, pembayaran) 
+INSERT INTO restaurant (pelanggan, makanan, jumlah_makanan, minuman, jumlah_minuman, metode, no_tempat, total_harga, pembayaran) 
 VALUES
     ('dr. Nurita', 'Mie Ayam Spesial', '2', 'Air Putih', '4', 'Diantar', 'Kamar 203', '60.000', 'Tunai'),
     ('Aldenia Boo', 'Nasi Goreng Udang, Mie Ayam Bakso', '1, 2', 'Es Jeruk'], '3', 'Ditunggu', 'Meja 8', '84.000', 'ATM');
